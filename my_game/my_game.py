5c7d263a15097546d99293728d20a1da912c3ea5
Karin Levenstein <karinlee@alumni.rice.edu>
2024-10-30 14:18:38 -0500

    More loot shenanigans

--- my_game/my_game.py
+++ my_game/my_game.py
@@ -2,19 +2,21 @@ import os
 
 def fight():
     print("Your opponent attacks you with everything they have!")
-    fight_response = input("Do you [F]ight back or [R]un away?")
+    fight_response = input("Do you [F]ight back or [R]un away? > ")
     if fight_response in ["fight", "f"]:
         print("You are too easily overpowered by the guard. If you keep fighting, you will probably lose.")
-        fight_continue = input("You can [C]ontinue to fight, or [R]un away")
+        fight_continue = input("You can [C]ontinue to fight, or [R]un away. > ")
         if fight_continue in ["continue", "c"]:
             print("The guard knocks you down and the last thing you see is a sword descending toward your face.")
             end_adventure_dead()
         if fight_continue in ["run", "r"]:
+            print("Discretion is the better part of valour.")
             print("You get away! ... and end up where you started.")
-            start_adventure()
+            choose_door()
     elif fight_response in ["run", "r"]:
+        print("The best fights are the ones you don't start.")
         print("You get away! ... and end up where you started.")
-        start_adventure()
+        choose_door()
 
 def treasure_chest():
     import game_dict
@@ -22,14 +24,14 @@ def treasure_chest():
     print("Inside, you see the following items:")
     for x in game_dict.treasure:
         print(x)
-    loot = input("Will you take the [G]old, the [C]up, the [R]ing, or the [S]word?")
+    loot = input("Will you take the [G]old, the [C]up, the [R]ing, or the [S]word? > ")
     if loot in ["gold", "g"]:
         print("The coins are shiny and smooth. You put them in your pocket.")
         game_dict.treasure.remove("Gold coins")
         blue_room()
     elif loot in ["cup", "c"]:
         print("You see now that the cup is actually badly tarnished. In fact, it's only plate.")
-        loot_cup = input("Do you [L]eave it or [T]ake it? >")
+        loot_cup = input("Do you [L]eave it or [T]ake it? > ")
         if loot_cup in ["leave", "l"]:
             print("You put it back.")
             blue_room()
@@ -40,8 +42,16 @@ def treasure_chest():
     elif loot in ["ring", "r"]:
         print("The ring is curiously heavy for its size.")
         print("You feel compelled to stick it in your pocket.")
-        print("You do so, and leave.")
+        print("You do so, and feel uneasy. Maybe you should put it back?")
         game_dict.treasure.remove("A diamond ring")
+        loot_ring = input("Do you [R]eturn the ring, or [K]eep it? > ")
+        if loot_ring in ["return", "r"]:
+            print("You put the ring back and feel better.")
+            game_dict.treasure.add("A diamond ring")
+        if loot_ring in ["keep", "k"]:
+            print("You decide to keep the ring anyway.")
+            print("After all, why shouldn't you keep it?")
+            print("It's yours now. Your precious.")
         blue_room()
     elif loot in ["sword", "s"]:
         game_dict.treasure.remove("A steel sword")
@@ -53,18 +63,18 @@ def treasure_chest():
 
 def red_room():
     print("It's a red room.")
-    print("It's not finished yet.")
-    start_adventure()
+    print("It's not finished yet. Go back.")
+    choose_door()
 
 def yellow_room():
     print("You pass through the door ... and end up where you started.")
-    start_adventure()
+    choose_door()
 
 def blue_room():
     
     print("You are inside a room where the walls, ceiling, and floor are all painted blue.")
     print("There is a treasure chest to the right and a sleeping goblin guard to the left.")
-    blue_action = input("Do you [L]eave, [O]pen the treasure chest, or [A]ttack the guard?")
+    blue_action = input("Do you [L]eave, [O]pen the treasure chest, or [A]ttack the guard? > ")
     if blue_action in ["leave", "l"]:
         start_adventure()
     elif blue_action in ["open", "o"]:
@@ -86,7 +96,7 @@ def start_adventure():
     choose_door()
 
 def choose_door():
-    door_picked = input("Which door do you pick? [R]ed, [B]lue, [Y]ellow], or [E]ntrance >")
+    door_picked = input("Which door do you pick? [R]ed, [B]lue, [Y]ellow], or [E]ntrance > ")
     if door_picked in ["red","r"]:
         red_room()
     elif door_picked in ["blue","b"]:
@@ -121,7 +131,7 @@ def get_player_name():
         name = alt_name
         print(f"Aha, a fellow Baldur's Gate fan. Let's go, {name}!")
     elif answer.lower() in ["n","no"]:
-        print("Okay, fine, {name}. Killjoy. Let's go anyway.")
+        print(f"Okay, fine, {name}. Killjoy. Let's go anyway.")
     else:
         print(f"Very funny, {alt_name}. Let's go.")
         name = alt_name
